f0d6117adf0b806f3fc35a6490e54949c29a340f
Alex Shepel <iam.alex.shepel@gmail.com>
2021-12-30 18:21:12 +0200

    prettify header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, getIsLoggedIn, getUsername, getIsAuthing } from 'redux/auth';
 import s from './Header.module.css';
+import { AiOutlineUser } from 'react-icons/ai';
 import Spinner from '../Spinner';
 
 const Header = () => {
@@ -13,8 +14,11 @@ const Header = () => {
     <div className={s.header}>
       <h1>Phonebook</h1>
       {isLoggedIn && (
-        <div className={s.user}>
-          <span>{name}</span>
+        <div className={s.panel}>
+          <div className={s.user}>
+            <AiOutlineUser size={22} />
+            <span>{name}</span>
+          </div>
           <div className={s.logoutContainer}>
             {isAuthing && <Spinner />}
             <button
